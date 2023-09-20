26a4f8b7dca6ac6cff4598ebd8f32872f67fc00c
spike80 <fabriciojnuario@live.com>
2023-09-20 15:30:05 -0300

    version 1.0.1

---
+++ spike80/access/Index.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import spike80.view.RouterView as rv
+
+
+def login_pass():
+    rv.RouterView()
+
+
+class Login:
+    def __init__(self, win):
+        self.left_frame = tk.Frame(root, width=200, height=300, bg="MIDNIGHTBLUE", relief="raised")
+        self.left_frame.pack(side=LEFT)
+
+        self.right_frame = tk.Frame(root, width=399, height=300, bg="MIDNIGHTBLUE", relief="raised")
+        self.right_frame.pack(side=RIGHT)
+
+        # self.logo_label = tk.Label(left_frame, image=logo, bg="MIDNIGHTBLUE")
+        # self.logo_label.place(x=50, y=100)
+
+        self.user_label = tk.Label(self.right_frame, text="username:", font=("Century Gothic", 10),
+                                   bg="MIDNIGHTBLUE", fg="white")
+        self.user_label.place(x=40, y=60)
+
+        self.user_entry = ttk.Entry(self.right_frame, width=30)
+        self.user_entry.place(x=120, y=60)
+
+        self.psw_label = tk.Label(self.right_frame, text="password:", font=("Century Gothic", 10),
+                                  bg="MIDNIGHTBLUE", fg="white")
+        self.psw_label.place(x=40, y=100)
+
+        self.psw_entry = ttk.Entry(self.right_frame, width=30, show="*")
+        self.psw_entry.place(x=120, y=100)
+
+        self.text = tk.StringVar()
+        self.text.set("login")
+
+        self.btn_login = ttk.Button(self.right_frame, text="login", width=20,command=login_pass)
+        self.btn_login.place(x=10, y=200)
+
+        self.btn_register = ttk.Button(self.right_frame, text="register", width=20, command=None)
+        self.btn_register.place(x=220, y=200)
+
+
+root = tk.Tk()
+Login(root)
+root.title('Paradase Hotel AccessPanel')
+
+window_width = 600
+window_height = 300
+
+# get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# find the center point
+center_x = int(screen_width / 2 - window_width / 2)
+center_y = int(screen_height / 2 - window_height / 2)
+
+# set the position of the window to the center of the screen
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+root.resizable(False, False)
+root.attributes('-alpha', 0.5)
+root.mainloop()
---
+++ spike80/view/RouterView.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+
+class RouterView:
+    def __init__(self):
+        win = tk.Toplevel(bg='MIDNIGHTBLUE', width=600, height=600, relief="raised")
+        win.title("Paradise Hotel Home")
