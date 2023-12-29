61f458aac903ecca8d0fd1df7fb68d94fb8130da
spike80 <fabriciojnuario@live.com>
2023-12-29 18:28:23 -0300

    version 1.0.1

--- spike80/dao/DaoHostage.py
+++ spike80/dao/DaoHostage.py
@@ -1,141 +1,130 @@
 import psycopg2
 
-from DaoConnection import DaoConnection
-from spike80.domain.Hostage import Hostage
+import spike80.dao.DaoConnection as dc
+import spike80.resource.Access as ac
 
 
 class Hostage:
     def __int__(self):
-        self.hostage = Hostage()
-        self.connection = DaoConnection()
+        print('Contructor method')
 
     def listaHospedagem(self):
+        hostages = []
         try:
-            cursor = self.connection.cursor()
+            connection = dc.DaoConnection().get_connection(ac.name, ac.psw)
+            cursor = connection.cursor()
             sql_select_query = """ select * from public."hospedagem"""""
             cursor.execute(sql_select_query)
             registers = cursor.fetchall()
-            for registry in registers:
-                self.hostage.id_hostage = registry[0]
-                self.hostage.id_c = registry[1]
-                self.hostage.nroom = registry[2]
-                self.hostage.dcheckin = registry[3]
-                self.hostage.dcheckout = registry[4]
-                self.hostage.status = registry[5]
+            for i in range(len(registers)):
+                hostages.append(registers[i])
+
             print(registers)
-            print(str(self.hostage))
 
         except(Exception, psycopg2.Error) as error:
-            if self.connection:
+            if connection:
                 print(f"Error in selection operation\n", error)
             else:
                 print(f"No database connection\n")
 
         finally:
-            if self.connection:
+            if connection:
                 cursor.close()
-                self.connection.close()
+                connection.close()
                 print(f"Connection closed\n")
 
-        return self.hostage
+        return hostages
 
     def selecionaHospedagem(self, id_hospedagem):
         try:
-            cursor = self.connection.cursor()
+            connection = dc.DaoConnection().get_connection(ac.name, ac.psw)
+            cursor = connection.cursor()
             sql_select_query = """ select * from public."hospedagem" where 
                                     "id_hospedagem" = %s """
-            cursor.execute(sql_select_query, id_hospedagem)
+            cursor.execute(sql_select_query, id_hospedagem, )
             registry = cursor.fetchone()
-            self.hostage.id_hostage = registry[0]
-            self.hostage.id_c = registry[1]
-            self.hostage.nroom = registry[2]
-            self.hostage.dcheckin = registry[3]
-            self.hostage.dcheckout = registry[4]
-            self.hostage.status = registry[5]
 
-            print(str(self.hostage))
 
         except(Exception, psycopg2.Error) as error:
-            if self.connection:
+            if connection:
                 print(f"Error in select operation\n", error)
             else:
                 print(f"No connection\n", error)
 
         finally:
-            if self.connection:
+            if connection:
                 cursor.close()
-                self.connection.close()
-                print(f"Connection closed\n")
+                connection.close()
+                print("Connection closed\n")
 
-        return self.hostage
+        return registry
 
-    def adicionaHospedagem(self):
+    def adicionaHospedagem(self, rg_cliente, num_quarto):
         try:
-            cursor = self.connection.cursor()
-            record_to_insert = (self.rg_cliente, self.num_quarto)
+            connection = dc.DaoConnection().get_connection(ac.name, ac.psw)
+            cursor = connection.cursor()
+            record_to_insert = (rg_cliente, num_quarto)
             cursor.callproc('adicionahospedagem', record_to_insert)
-            self.connection.commit()
+            connection.commit()
             count = cursor.rowcount
-            print(count, f" Registro inserido com sucesso")
+            print(count, "Registro incluido com sucesso")
 
         except(Exception, psycopg2.Error) as error:
-            if self.connection:
-                print(f"Não foi possível adicionar hospedagem\n", error)
+            if connection:
+                print("Não foi possível adicionar hospedagem\n", error)
             else:
                 print(f"No database connection.")
 
         finally:
-            if self.connection:
+            if connection:
                 cursor.close()
-                self.connection.close()
+                connection.close()
                 print(f"Connection closed.")
 
-    def excluiHospedagem(self):
+    def excluiHospedagem(self, id_hospedagem):
         try:
-            cursor = self.connection.cursor()
+            connection = dc.DaoConnection().get_connection(ac.user, ac.psw)
+            cursor = connection.cursor()
             sql_delete_query = """ delete from public."hospedagem" 
                                    where id_hospedagem = %s"""
 
-            cursor.execute(sql_delete_query, (self.id_hospedagem,))
-            self.connection.commit()
+            cursor.execute(sql_delete_query, (id_hospedagem,))
+            connection.commit()
             count = cursor.rowcount
-            print(f"Registro excluido com sucesso.")
+            print(count, " Registro(s) excluido com sucesso.")
 
         except(Exception, psycopg2.Error) as error:
-            if self.connection:
+            if connection:
                 print("Erro ao excluir o registro", error)
             else:
-                print(f"No connection. ", error)
+                print("No connection. ", error)
 
         finally:
-            if self.connection:
+            if connection:
                 cursor.close()
-                self.connection.close()
+                connection.close()
                 print(f"Connection closed.")
 
-    def atualizaHospedagem(self):
+    def atualizaHospedagem(self, id_hospedagem, rg_cliente, num_quarto, dt_entrada, dt_saida, status):
         try:
-            self.abrirConexao()
-            cursor = self.connection.cursor()
-            record_to_insert = (self.id_hospedagem, self.rg_cliente,
-                                self.num_quarto, self.dt_entrada, self.dt_saida,
-                                self.status)
-            sql_update_query = """ update public."hospedagem" set "id_hospedagem" = %s,
-                                "rg" = %s, "num_quarto" = %s, "dt_entrada" = %s, "dt_saida" = %s,
-                                "status" = %s """
+            connection = dc.DaoConnection.get_connection(ac.name, ac.psw)
+            cursor = connection.cursor()
+            record_to_insert = (rg_cliente, a)
+            sql_update_query = """ update public."hospedagem" set  "rg" = %s, "num_quarto" = %s, "dt_entrada" = %s, "dt_saida" = %s,
+                                "status" = %s where "id_hospedagem" = s% """
             cursor.execute(sql_update_query, record_to_insert)
-            self.connection.commit()
+            connection.commit()
 
             print(f"Registro atualizado com sucesso.")
 
         except(Exception, psycopg2.Error) as error:
-            if self.connection:
+            if connection:
                 print("Erro ao atualizar o registro.", error)
             else:
                 print("No connection.", error)
 
         finally:
-            if self.connection:
+            if connection:
                 cursor.close()
-                self.connection.close()
-                print(f"Connection closed.")
+                connection.close()
+                print("Connection closed.")
